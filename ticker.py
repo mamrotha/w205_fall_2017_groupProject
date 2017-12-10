cbf74566226881ae8f924090503a0eb05a318e3b
robodeng <deng.bobby@gmail.com>
2017-12-10 01:18:56 +0000

    Rob Updated with Cron working

---
+++ ticker.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+import numpy as np
+import datetime as dt
+import psycopg2
+import warnings
+from sqlalchemy import create_engine
+
+warnings.filterwarnings("ignore")
+
+r = requests.get("https://api.coinmarketcap.com/v1/ticker/?limit=0")
+ticker = pd.DataFrame(r.json())
+ticker['last_updated'] = pd.to_datetime(ticker['last_updated'],unit='s')
+ticker['date'] = [d.date() for d in ticker["last_updated"]]
+ticker['time'] = [d.time() for d in ticker["last_updated"]]
+ticker = ticker[["date", "time", "id", "symbol", "price_usd", "available_supply", "total_supply"]]
+
+
+try:
+    # CREATE DATABASE can't run inside a transaction
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute("CREATE DATABASE crypto")
+    cur.close()
+    conn.close()
+except:
+    print ("Database Already Created")
+
+engine = create_engine('postgresql+psycopg2://postgres:foobar@localhost:5432/crypto')
+ticker.to_sql("ticker_data", engine, if_exists="append")
