9fe2e3ee5072999b60d20b41b425fb76f06896b7
mamrotha <andrew_mamroth@berkeley.edu>
2017-12-14 09:06:32 -0500

    updated ticker_v2.py

--- ticker_v2.py
+++ ticker_v2.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import psycopg2
+from sqlalchemy import create_engine
 
 #Connect to the database and create cursor
 conn = psycopg2.connect(database="crypto", user="postgres",           host="localhost", port="5432")
@@ -17,7 +18,8 @@ ticker['time'] = [d.time() for d in ticker["last_updated"]]
 ticker = ticker[["date", "time", "id", "symbol", "price_usd", "available_supply", "total_supply"]]
 
 #Insert data into appropriate table
-cur.execute("INSERT INTO ticker (date, time, id, symbol, price_usd, 	available_supply, total_supply) VALUES (%s,%s,%s,%s,%s,%s,%s,)", 		(date, time, id, symbol, price_usd, available_supply, 	total_supply,))
+engine = create_engine('postgresql+psycopg2://postgres:foobar@localhost:5432/crypto')
+ticker.to_sql("ticker_data", engine, if_exists="append")
 
 conn.commit()
 conn.close()
