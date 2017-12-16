20cdceb6f3759a4b2bd511b691fdabbd2128f814
mamrotha <andrew_mamroth@berkeley.edu>
2017-12-16 15:46:28 -0500

    updates

--- bitcoin_table.py
+++ bitcoin_table.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import psycopg2
+import datetime as dt
 from sqlalchemy import create_engine
 
 #Connect to database and create engine
@@ -11,9 +12,10 @@ engine = create_engine('postgresql+psycopg2://postgres:foobar@localhost:5432/cry
 
 #Create dataframe of bitcoin data
 sql_1 = "SELECT date, time, price_usd \
-       FROM ticker \
-       WHERE id = 'bitcoin';"
+         FROM ticker \
+         WHERE id = 'bitcoin';"
 df = pd.read_sql_query(sql_1, engine)
+df['datetime'] = dt.combine(df['date'],df['time'])
 
 #Get last hours of tweet sentiments
 times = list(df['time'])
