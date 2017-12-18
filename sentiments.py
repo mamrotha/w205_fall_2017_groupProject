ce7a5b5cb2342caccf94162398e61cf899bc02c1
mamrotha <andrew_mamroth@berkeley.edu>
2017-12-18 17:21:25 -0500

    removed unnecessary files

--- sentiments.py
+++ sentiments.py
@@ -33,33 +33,3 @@ df['sentiment'] = np.asarray(sentiments)
 
 #Send dataframe to new table
 df.to_sql("tweet_sentiment", engine, if_exists="append")
-
-'''
-2017-12-14 16:00:00+00:00
-'%Y-%m-%d %H:%M:%S+00:00'
-
-Sat Dec 16 13:59:32 +0000 2017
-'%a %b %d %H:%M:%S +0000 %Y'
-
-SELECT DISTINCT created_at_hour
-FROM tweets_hr_adjust
-ORDER BY created_at_hour ASC
-
-tweets_hr_adjust
-2017-12-07 22:00:00+00
-2017-12-15 04:00:00+00
-
-SELECT DISTINCT update_hour
-FROM bitcoin_hr_adjust
-ORDER BY update_hour ASC
-
-bitcoin_hr_adjust
-2017-12-11 06:00:00+00
-2017-12-14 18:00:00+00
-
-SELECT created_at_hour, tweet
-FROM tweets_hr_adjust
-WHERE lang = 'en' AND
-created_at_hour between '12/11/2017' and '12/12/2017'
-
-'''
